0c9a7bd92ace6aa4be40520ca041d76d18092679
Vinglish <khan.ildar94@gmail.com>
2019-04-16 16:39:13 +0300

    views for Order

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+from .models import Order, OrderItem
+
+
+class OrderCreateView(CreateView):
+    model = Order
+
+
+class OrderItemCreateView(CreateView):
+    model = OrderItem
