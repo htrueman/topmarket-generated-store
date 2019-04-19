8eeeb994be61c67ea660d3a6065e201893be617f
Vinglish <khan.ildar94@gmail.com>
2019-04-19 16:19:11 +0300

    api

---
+++ shop/constants.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext as _
+
+DOMEN = (
+    ('DM', _("Домен")),
+    ('SD', _('Поддомен'))
+)
+
+CALL_BACK = (
+    ('YES', _('Включена')),
+    ('NO', _('Выключена'))
+)
+
+USER_ROLE = (
+    ('CONTRACTOR', _('Поставщик')),
+    ('PARTNER', _('Партнер')),
+)
--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext as _
 
 # Single tone
+from shop.constants import CALL_BACK, DOMEN
 
 
 class SingletonModel(models.Model):
@@ -31,6 +33,48 @@ class SliderMainPage(models.Model):
     class Meta:
         verbose_name = _('Ссылка на картинку')
         verbose_name_plural = _('Ссылки на картинку')
+
+
+class MyStore(models.Model):
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+
+    domain_subdomain = models.CharField(max_length=2, choices=DOMEN, blank=True, null=True, verbose_name=_('Домен/поддомен'))
+    domain_name = models.URLField(max_length=200, null=True, blank=True, verbose_name=_('Имя домена'))
+    call_back = models.CharField(max_length=3, choices=CALL_BACK, null=True, blank=True, verbose_name=_('Функция Сall-back'))
+    facebook = models.URLField(max_length=200, null=True, blank=True, verbose_name=_('Facebook'))
+    instagram = models.URLField(max_length=200, null=True, blank=True, verbose_name=_('Instagram'))
+    linkedin = models.URLField(max_length=200, null=True, blank=True, verbose_name=_('Linkedin'))
+    top_sales = models.BooleanField(default=False, verbose_name='Топ продаж')
+    no_items = models.BooleanField(default=False, verbose_name='Без товара')
+    logo = models.ImageField(upload_to='users/company_logo', null=True, blank=True, verbose_name=_('Логотип'))
+
+    @property
+    def get_url(self):
+        if self.domain_subdomain:
+            return 'https://{}/'.format(self.domain_name)
+
+
+class HeaderPhoneNumber(models.Model):
+
+    store = models.ForeignKey('MyStore', on_delete=models.CASCADE, related_name='header_phones', null=True, blank=True)
+    number = models.CharField(max_length=20, null=True, blank=True, verbose_name=_('Номер телефона для хедера'))
+
+
+class FooterPhoneNumber(models.Model):
+
+    store = models.ForeignKey('MyStore', on_delete=models.CASCADE, related_name='footer_phones', null=True, blank=True)
+    number = models.CharField(max_length=20, null=True, blank=True, verbose_name=_('Номер телефона для футера'))
+
+
+class Navigation(models.Model):
+    store = models.OneToOneField('MyStore', on_delete=models.CASCADE, related_name='navigations', null=True, blank=True)
+    navigation = models.CharField(max_length=200, null=True, blank=True, verbose_name=_('Раздел навигации'))
+
+
+class StoreSliderURL(models.Model):
+    store = models.ForeignKey(MyStore, on_delete=models.CASCADE, related_name='slider_urls')
+    image = models.URLField(null=True, blank=True, verbose_name='Картинка для слайдера')
 
 
 class LogoWithPhones(SingletonModel):
