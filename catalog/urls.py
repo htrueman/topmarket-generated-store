290d2670bc265a3d185e7bfddc54650c7d823aa9
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-17 14:25:18 +0300

    Started back + front sync

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
+
+app_name = 'catalog'
 
 urlpatterns = [
-
+    path('products/', ProductListView.as_view(), name='products_list'),
+    path('products/category/<int:category_id>', ProductListByCategoryView.as_view(), name='products_by_category'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
 
 from .views import LogoWithPhonesView, SocialNetworkView, DeliveryAndPaymentView, ExchangeAndReturnView, HowToUseView, \
-    SupportView, ContactsView, AboutUsView
+    SupportView, ContactsView, AboutUsView, HomePageView
 
 app_name = 'shop'
 
 
 urlpatterns = [
-    path('logo_with_phones/', LogoWithPhonesView.as_view, name='logo_with_phones_url'),
-    path('social_network/', SocialNetworkView.as_view, name='social_network_url'),
-    path('delivery_and_payment/', DeliveryAndPaymentView.as_view, name='delivery_and_payment_url'),
-    path('exchange_and_return/', ExchangeAndReturnView.as_view, name='exchange_and_return_url'),
-    path('how_to_use/', HowToUseView.as_view, name='how_to_use_url'),
-    path('support/', SupportView.as_view, name='support_url'),
-    path('contacts/', ContactsView.as_view, name='contacts_url'),
-    path('about_us/', AboutUsView.as_view, name='about_us_url')
-
+    path('', HomePageView.as_view(), name='index'),
+    path('logo_with_phones/', LogoWithPhonesView.as_view(), name='logo_with_phones_url'),
+    path('social_network/', SocialNetworkView.as_view(), name='social_network_url'),
+    path('delivery_and_payment/', DeliveryAndPaymentView.as_view(), name='delivery_and_payment_url'),
+    path('exchange_and_return/', ExchangeAndReturnView.as_view(), name='exchange_and_return_url'),
+    path('how_to_use/', HowToUseView.as_view(), name='how_to_use_url'),
+    path('support/', SupportView.as_view(), name='support_url'),
+    path('contacts/', ContactsView.as_view(), name='contacts_url'),
+    path('about_us/', AboutUsView.as_view(), name='about_us_url')
 ]
